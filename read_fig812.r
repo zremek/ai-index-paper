11a6ac76b8d5c598759e4fe67be294c82732f654
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2025-12-15 15:34:32 +0100

    add plots

--- read_fig812.r
+++ read_fig812.r
@@ -20,7 +20,8 @@ cntry_2025 <- c("Indonesia", "Thailand", "Singapore", "Mexico", "Malaysia", "Per
   "Ireland", "Japan", "Australia", "Switzerland", "Netherlands", 
   "Great Britain", "Sweden", "United States", "Belgium", "France", "Canada")
 
-vals_2025 <- c(85, 78, 73, 70, 70, 69, 67, 67, 66, 64, 63, 58, 58, 57, 53, 53, 50, 49, 48, 48, 48, 47, 45, 43, 43, 43,42, 41, 41, 40)
+vals_2025 <- c(85, 78, 73, 70, 70, 69, 67, 67, 66, 64, 63, 58, 58, 57, 53, 53, 
+               50, 49, 48, 48, 48, 47, 45, 43, 43, 43,42, 41, 41, 40)
 
 ipsos_2025 <- tibble(cntry = cntry_2025, 
                 year = 2025, 
@@ -45,6 +46,7 @@ ggplot(fig812, aes(x = year, y = pct)) +
   scale_x_continuous(breaks = 2022:2024) + 
   labs(title = "Products and services using AI have more benefits\nthan drawbacks [% 'Agree'], Ipsos")
 
+# 22-25
 ggplot(ipsos, aes(x = year, y = pct)) + 
   geom_point() +
   geom_line() + 
@@ -57,4 +59,16 @@ ggplot(fig812, aes(x = year, y = pct, colour = cntry)) +
   geom_line() + 
   scale_x_continuous(breaks = 2022:2024)
 
-ip
+# better 22-25?
+ggplot(ipsos, mapping = aes(x = reorder(cntry, pct, FUN = median, na.rm = T),
+                            y = pct, 
+                            colour = factor(year), 
+                            group = cntry)) + 
+  geom_point() + 
+  geom_line() +
+  coord_flip() +
+  labs(title = "Products and services using AI have more benefits\nthan drawbacks [% 'Agree'], Ipsos")
+
+
+
+
